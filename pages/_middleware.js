12c90135fe6739964ad8718bd3d5ca06cd58a42a
ankit <yadav.ankit@thinksys.com>
2022-01-24 12:32:16 +0530

    middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req, ev) {
-  return NextResponse.redirect(`https://www.tremendo.in/`, 301);
+export function middleware(req) {
+  if (req.headers.get("x-forwarded-proto") !== "https") {
+    return NextResponse.redirect(
+      `https://${req.nextUrl.hostname}${req.nextUrl.pathname}`,
+      301
+    );
+  }
+  return NextResponse.next();
 }
