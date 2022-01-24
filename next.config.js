0530a8651d08e4d8543ce2b611ed2d3981404e98
ankit <yadav.ankit@thinksys.com>
2022-01-24 11:15:11 +0530

    Add

--- next.config.js
+++ next.config.js
@@ -9,3 +9,9 @@ module.exports = {
     ]
   }
 };
+
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true"
+// });
+
+// module.exports = withBundleAnalyzer({});
