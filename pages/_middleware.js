08ba0ba56d1334f784360d5a6a2fed552bc40857
ankit <yadav.ankit@thinksys.com>
2022-03-04 11:56:27 +0530

    comment middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  if (!req.cookies["enableHTTPS"]) {
-    let httpResponse = NextResponse.redirect(
-      `https://${req.nextUrl.hostname}${req.nextUrl.pathname}`,
-      301
-    );
-    response.cookie("enableHTTPS", "true");
-    return httpResponse;
-  }
+  // if (!req.cookies["enableHTTPS"]) {
+  //   let httpResponse = NextResponse.redirect(
+  //     `https://${req.nextUrl.hostname}${req.nextUrl.pathname}`,
+  //     301
+  //   );
+  //   response.cookie("enableHTTPS", "true");
+  //   return httpResponse;
+  // }
   return NextResponse.next();
 }
