3be525c5b9a635c0ccb2ed93bead67a2bfd4524d
RaviAdvertising <94158432+RaviAdvertising@users.noreply.github.com>
2022-06-27 19:27:59 +0530

    Update DesktopOnly.js

--- components/DeviceCheck/DesktopOnly.js
+++ components/DeviceCheck/DesktopOnly.js
@@ -1,7 +1,14 @@
-import { useContext } from "react";
+import { useContext,useEffect, useState } from "react";
 import { DeviceContext } from "../../pages/_app";
 
 export default function DesktopOnly(props) {
   const { isMobileView } = useContext(DeviceContext);
-  return !isMobileView ? props.children : null;
+  const [mobile, setMobile] = useState(false);
+  useEffect(() => {
+    checkDevice();
+  }, []);
+  const checkDevice = () => {
+    if (window.innerWidth < 1024) setMobile(true);
+  };
+  return !isMobileView&& !mobile ? props.children : null;
 }
