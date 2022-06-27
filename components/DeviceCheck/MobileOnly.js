e0144f8065a85181d35714b6ba74294604e45033
RaviAdvertising <94158432+RaviAdvertising@users.noreply.github.com>
2022-06-27 19:28:53 +0530

    Update MobileOnly.js

--- components/DeviceCheck/MobileOnly.js
+++ components/DeviceCheck/MobileOnly.js
@@ -1,7 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState  } from "react";
 import { DeviceContext } from "../../pages/_app";
 
 export default function MobileOnly(props) {
   const { isMobileView } = useContext(DeviceContext);
-  return isMobileView ? props.children : null;
+   const [mobile, setMobile] = useState(true);
+  useEffect(() => {
+    checkDevice();
+  }, []);
+  const checkDevice = () => {
+    if (window.innerWidth < 1024) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  };
+  return isMobileView || mobile ? props.children : null;
 }
